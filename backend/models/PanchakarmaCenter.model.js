3f6cebd2be65907578384c94b5cc93005aec4e16
vedant-khasbage <vedantkhasbage2005@gmail.com>
2025-12-02 20:33:44 +0530

    add therapy , add doctor connected with backend

--- backend/models/PanchakarmaCenter.model.js
+++ backend/models/PanchakarmaCenter.model.js
@@ -1,31 +1,42 @@
-const { verify } = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-
-const PanchakarmaCenterSchema=new schema({
-  role:{
-    type:String,
-  default:"centerHead"
+const PanchakarmaCenterSchema = new schema({
+  role: {
+    type: String,
+    default: "centerHead",
   },
-    Adminname:String,
-    CenterName:String,
-    licenseNo:String,
-    password:String,
-    email:{
-        type:String,unique:true
-    },
+
+  Adminname: String,
+  CenterName: String,
+  licenseNo: String,
+  password: String,
+
+  email: {
+    type: String,
+    unique: true,
+  },
+
   location: { type: String },
   latitude: { type: Number },
-  longitude: { type: Number},
-  MobileNo:{type:String,required:true},
-  BotNumber:{
-    type:String
-  },
-   verified:{
-        type:Boolean,
-        default:false
-    }
-})
+  longitude: { type: Number },
 
-module.exports = mongoose.model("PanchakarmaCenter",PanchakarmaCenterSchema);
+  MobileNo: { type: String, required: true },
+
+  BotNumber: { type: String },
+
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+
+  // Store doctor references
+  Doctors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+    }
+  ],
+});
+
+module.exports = mongoose.model("PanchakarmaCenter", PanchakarmaCenterSchema);
