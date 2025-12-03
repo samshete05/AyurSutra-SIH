6c79685de783e9df29f821187e2254e331e8670b
samshete05 <samshete2005@gmail.com>
2025-12-04 01:10:19 +0530

    Update privacy policy and terms

---
+++ frontend/src/pages/HelpCenter.jsx
@@ -0,0 +1,9 @@
+const HelpCenter = () => {
+  return (
+    <div>
+      {/* Help Center content */}
+    </div>
+  );
+};
+
+export default HelpCenter;
--- frontend/src/router/approutes.jsx
+++ frontend/src/router/approutes.jsx
@@ -35,6 +35,9 @@ import { elements } from "chart.js";
 import PatientProtectedRoute from "../pages/patient/patientProtectedRoute";
 import ViewDoctors from "../pages/center/ViewDoctor";
 import ViewTherapists from "../pages/center/ViewTherapist";
+import TermsServices from "../pages/TermsServices";
+import Privacy from "../pages/privacy";
+import HelpCenter from "../pages/HelpCenter";
 // import TherapyBooking from "../pages/TherapyBooking";
 // import DoctorBooking from "../pages/DoctorBooking";
 export const AppRoutes = () => {
@@ -90,6 +93,9 @@ export const AppRoutes = () => {
         {/* <Route path="*" element={<NotFound />} /> */}
 
         <Route path="/center-map" element={<CenterMap/>}/>
+        <Route path="/terms-services" element={<TermsServices />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/help-center" element={<HelpCenter />} />
       </Routes>
     </div>
   );
